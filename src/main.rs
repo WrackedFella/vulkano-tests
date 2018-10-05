c824a035b18a846053556ed522149fb0ac58a2c3
jmoore5268 <jmoore5268@gmail.com>
2018-10-05 19:58:56 -0400

    Slight refactoring

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,18 @@ use event_manager::EventManager;
 use core::traits::Updateable;
 
 fn main() {
-    let mut managers: Vec<Box<Updateable>> = Vec::new();
-    managers.push(Box::new(SceneManager::new()));
-    managers.push(Box::new(ObjectManager::new()));
-    managers.push(Box::new(EventManager::new()));
+    let mut managers = init_managers();
     let mut i = 10;
     while i > 0 {
         managers.iter_mut().for_each(|m| { m.update() });
         i = i - 1;
     }
+}
+
+fn init_managers() -> Vec<Box<Updateable>> {
+    let mut managers: Vec<Box<Updateable>> = Vec::new();
+    managers.push(Box::new(SceneManager::new()));
+    managers.push(Box::new(ObjectManager::new()));
+    managers.push(Box::new(EventManager::new()));
+    return managers;
 }
