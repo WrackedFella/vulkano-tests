544aa5c7e896626065cbe5d612704c78ecdf3824
jmoore5268 <jmoore5268@gmail.com>
2018-10-05 19:56:28 -0400

    Added an event manager

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 extern crate core;
 extern crate scene_manager;
 extern crate object_manager;
+extern crate event_manager;
 
 use scene_manager::SceneManager;
 use object_manager::ObjectManager;
+use event_manager::EventManager;
+
 use core::traits::Updateable;
 
 fn main() {
-    let mut v: Vec<Box<Updateable>> = Vec::new();
-    v.push(Box::new(SceneManager::new()));
-    v.push(Box::new(ObjectManager::new()));
+    let mut managers: Vec<Box<Updateable>> = Vec::new();
+    managers.push(Box::new(SceneManager::new()));
+    managers.push(Box::new(ObjectManager::new()));
+    managers.push(Box::new(EventManager::new()));
     let mut i = 10;
     while i > 0 {
-        v.iter_mut().for_each(|manager| { manager.update() });
+        managers.iter_mut().for_each(|m| { m.update() });
         i = i - 1;
     }
 }
