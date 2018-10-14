f77d52fdf8d143b10fcd55cdb1258a7917cba1e9
Justin Moore <jmoore5268@gmail.com>
2018-10-14 16:16:19 -0400

    Loading OBJ Files (#2)
    * Added some reference code
    
    * Loading Objs, printing data

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,26 @@
 extern crate core;
 extern crate scene_manager;
-extern crate object_manager;
-extern crate event_manager;
+
+use std::path::Path;
 
 use scene_manager::SceneManager;
-use object_manager::ObjectManager;
-use event_manager::EventManager;
-
 use core::traits::Updateable;
 
 fn main() {
-    let mut managers = init_managers();
-    let mut i = 10;
-    while i > 0 {
-        managers.iter_mut().for_each(|m| { m.update() });
-        i = i - 1;
-    }
+    let path = Path::new("C:\\dev\\samples\\3d\\house\\model.obj");
+    let mut manager = SceneManager::new();
+    manager.load_file(path);
+    // let mut managers = init_managers();
+    // let mut i = 10;
+    // while i > 0 {
+    //     managers.iter_mut().for_each(|m| { m.update() });
+    //     i = i - 1;
+    // }
 }
 
 fn init_managers() -> Vec<Box<Updateable>> {
     let mut managers: Vec<Box<Updateable>> = Vec::new();
     managers.push(Box::new(SceneManager::new()));
-    managers.push(Box::new(ObjectManager::new()));
-    managers.push(Box::new(EventManager::new()));
     return managers;
 }
+
